bde7939bc228a0da58e06c56a733857195d40e64
Abdur Rafay <58258487+abdurrafay2@users.noreply.github.com>
2022-09-07 04:41:09 +0500

    otp complete
    screen left

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,11 +1,10 @@
 import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:whatsapp_ui/colors.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_ui/common/util/colors.dart';
 import 'package:whatsapp_ui/common/util/utils.dart';
 import 'package:whatsapp_ui/common/widgets/custom_button.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_ui/features/auth/controller/auth_controller.dart';
-import 'package:whatsapp_ui/features/auth/screens/otp_screen.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
   static const routeName = '/login-screen';
@@ -40,7 +39,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
     if (country != null && phoneNumber.isNotEmpty) {
       ref
           .read(authControllerProvider)
-          .signInWithPhone(context, '${country!.phoneCode}$phoneNumber');
+          .signInWithPhone(context, '+${country!.phoneCode}$phoneNumber');
     } else {
       showSnackBar(context: context, content: 'Fill out all the fields');
     }
@@ -52,45 +51,49 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Enter Your Phone Number"),
+        title: const Text('Enter your phone number'),
         elevation: 0,
         backgroundColor: backgroundColor,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          const Text("Whatsapp will need to verify your phone number."),
-          const SizedBox(
-            height: 10,
-          ),
-          TextButton(
-            onPressed: pickCountry,
-            child: const Text("Pick Country"),
-          ),
-          const SizedBox(height: 5),
-          Row(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              if (country != null) Text('+${country!.phoneCode}'),
-              const SizedBox(width: 10),
+              const Text('WhatsApp will need to verify your phone number.'),
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: pickCountry,
+                child: const Text('Pick Country'),
+              ),
+              const SizedBox(height: 5),
+              Row(
+                children: [
+                  if (country != null) Text('+${country!.phoneCode}'),
+                  const SizedBox(width: 10),
+                  SizedBox(
+                    width: size.width * 0.7,
+                    child: TextField(
+                      controller: phoneController,
+                      decoration: const InputDecoration(
+                        hintText: 'phone number',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: size.height * 0.1),
               SizedBox(
-                width: size.width * 0.7,
-                child: TextField(
-                  controller: phoneController,
-                  decoration: const InputDecoration(
-                      hintText: "Enter your phone number"),
+                width: 90,
+                child: CustomButton(
+                  onPressed: sendPhoneNumber,
+                  text: 'NEXT',
                 ),
-              )
+              ),
             ],
           ),
-          SizedBox(height: size.height * 0.1),
-          SizedBox(
-            width: 90,
-            child: CustomButton(
-              onPressed: sendPhoneNumber,
-              text: ('NEXT'),
-            ),
-          )
-        ]),
+        ),
       ),
     );
   }
